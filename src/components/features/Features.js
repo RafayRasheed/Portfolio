4546ee9b974568eaac47e10e846a7349e4693eea
Rafay Rasheed <rafayrasheed777.rr@gmail.com>
2024-09-11 16:34:55 +0500

    adseg

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -6,7 +6,7 @@ const Features = () => {
   return (
     <section id="features" className="w-full py-10 ">
       <div className="flex justify-center items-center text-center">
-        <Title title="Features" des="What I Do" />
+        <Title title="Expertise" des="What I Do" />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-20">
         {featuresData.map((item) => (
